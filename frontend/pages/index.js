1b622116aeaaca870b01257f10c7264891d43ee0
Alek Novkovski <alek@greatcode2.com>
2023-02-21 10:17:31 +0100

    Prettier reformat

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,11 @@
 import Layout from "@/components/Layout";
 
 export default function HomePage() {
-  return (
-      <Layout title="The home page">
-        <h1>Home Page</h1>
-        <p>This is an app to find the latest DJ and other musical events</p>
-        <p>Version: 1.0.0</p>
-      </Layout>
-  )
+    return (
+        <Layout title="The home page">
+            <h1>Home Page</h1>
+            <p>This is an app to find the latest DJ and other musical events</p>
+            <p>Version: 1.0.0</p>
+        </Layout>
+    )
 }
