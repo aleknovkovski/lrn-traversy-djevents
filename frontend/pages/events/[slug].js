b0e90e5f635b6255efc39468a2903bb7e9d23096
Alek Novkovski <alek@greatcode2.com>
2023-02-21 16:00:37 +0100

    Build out singleEvent markup

--- frontend/pages/events/[slug].js
+++ frontend/pages/events/[slug].js
@@ -1,10 +1,45 @@
 import Layout from "@/components/Layout";
 import {API_URL} from "@/config";
+import classes from './[slug].module.css'
+import Link from "next/link";
+import Image from 'next/image'
+import {FaPencilAlt, FaTimes} from "react-icons/fa";
 
 export default function EventPage({ evt }) {
     return (
         <Layout>
-            <h1>{evt.name}</h1>
+            <div className={classes.event}>
+                <div className={classes.controls}>
+                    <Link href={`/events/edit/${evt.id}`}>
+                            <FaPencilAlt /> Edit Event
+
+                    </Link>
+                    <a href='#' className={classes.delete}>
+                        <FaTimes /> Delete Event
+                    </a>
+                </div>
+
+                <span>
+          {evt.date} at {evt.time}
+        </span>
+                <h1>{evt.name}</h1>
+                {evt.image && (
+                    <div className={classes.image}>
+                        <Image src={evt.image} width={960} height={600} />
+                    </div>
+                )}
+
+                <h3>Performers:</h3>
+                <p>{evt.performers}</p>
+                <h3>Description:</h3>
+                <p>{evt.description}</p>
+                <h3>Venue: {evt.venue}</h3>
+                <p>{evt.address}</p>
+
+                <Link href='/events' className={classes.back}>
+                    {'<'} Go Back
+                </Link>
+            </div>
         </Layout>
     )
 }
