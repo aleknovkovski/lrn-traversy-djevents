3c56198fe57281831cc4e5eb836ba67c833f8157
Alek Novkovski <alek@greatcode2.com>
2023-02-21 10:25:48 +0100

    Add EventItem component

---
+++ frontend/components/EventItem.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import classes from './EventItem.module.css'
+
+export default function EventItem({ evt }) {
+    return (
+        <div className={classes.event}>
+            <div className={classes.img}>
+                <Image
+                    src={evt.image ? evt.image : '/images/event-default.png'}
+                    width={170}
+                    height={100}
+                    alt={evt.name}
+                />
+            </div>
+
+            <div className={classes.info}>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+                <h3>{evt.name}</h3>
+            </div>
+
+            <div className={classes.link}>
+                <Link href={`/events/${evt.slug}`} className='btn'>
+                    Details
+                </Link>
+            </div>
+        </div>
+    )
+}
