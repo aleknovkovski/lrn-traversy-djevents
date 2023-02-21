a3a3819f09a4ef08230fce77cdedbf49d7d4e091
Alek Novkovski <alek@greatcode2.com>
2023-02-21 10:18:01 +0100

    Fetch 3 latest events from /api/events, log on frontend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,8 @@
 import Layout from "@/components/Layout";
+import {API_URL} from "@/config";
 
-export default function HomePage() {
+export default function HomePage(props) {
+    console.log(props)
     return (
         <Layout title="The home page">
             <h1>Home Page</h1>
@@ -8,4 +10,14 @@ export default function HomePage() {
             <p>Version: 1.0.0</p>
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json()
+
+    return {
+        props: { events: events.slice(0, 3) },
+        revalidate: 1,
+    }
 }
