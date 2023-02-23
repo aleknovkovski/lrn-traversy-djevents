32cefad74694aea32d43d00fdedb6cff41baf842
Alek Novkovski <alek@greatcode2.com>
2023-02-24 00:31:37 +0100

    Update search page JSX

--- frontend/pages/events/search.js
+++ frontend/pages/events/search.js
@@ -2,12 +2,18 @@ import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
 import { API_URL } from '@/config'
 import qs from "qs";
+import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function EventsPage({ events }) {
+    const router = useRouter()
+
     return (
-        <Layout>
-            <h1>Events</h1>
+        <Layout title='Search Results'>
+            <Link href='/events'>Go Back</Link>
+            <h1>Search Results for {router.query.term}</h1>
             {events.length === 0 && <h3>No events to show</h3>}
+
 
             {events.map((evt) => (
                 <EventItem key={evt.id} evt={evt.attributes} />
