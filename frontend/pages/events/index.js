06a0af449a173454477006f393ab0f922a730e99
Alek Novkovski <alek@greatcode2.com>
2023-02-26 17:13:49 +0100

    Add pagination Nav

--- frontend/pages/events/index.js
+++ frontend/pages/events/index.js
@@ -1,8 +1,10 @@
 import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
 import { API_URL } from '@/config'
+import Pagination from "@/components/Pagination";
+const PER_PAGE = 2
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total, PER_PAGE }) {
     return (
         <Layout>
             <h1>Events</h1>
@@ -11,18 +13,19 @@ export default function EventsPage({ events }) {
             {events.map((evt) => (
                 <EventItem key={evt.id} evt={evt.attributes} />
             ))}
+
+            <Pagination page={page} total={total} PER_PAGE={PER_PAGE}/>
         </Layout>
     )
 }
 
 export async function getServerSideProps({ query: { page = 1 } }) {
-    const PER_PAGE = 2
 
     // Fetch events
     const res = await fetch(`${API_URL}/events?populate=*&pagination[page]=${page}&pagination[pageSize]=${PER_PAGE}`)
     const events = await res.json()
 
     return {
-        props: { events: events.data}
+        props: { events: events.data, page: +page, total: events.meta.pagination.total, PER_PAGE}
     }
 }
