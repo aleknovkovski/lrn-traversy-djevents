38ffaaa71b8c1eef6af0e024feea20b4e408d79f
Alek Novkovski <alek@greatcode2.com>
2023-02-26 20:01:15 +0100

    Make nav elements dependant on auth status

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 import classes from './Header.module.css'
 import Search from "@/components/Search";
-import {FaSignInAlt} from "react-icons/fa";
+import {FaSignInAlt, FaSignOutAlt} from "react-icons/fa";
+import {useContext} from "react";
+import AuthContext from "@/context/AuthContext";
 
 export default function Header() {
+    const { user, logout } = useContext(AuthContext)
+
     return (
         <header className={classes.header}>
             <div className={classes.logo}>
@@ -19,16 +23,38 @@ export default function Header() {
                             Events
                         </Link>
                     </li>
-                    <li>
-                        <Link href='/events/add'>
-                            Add Event
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='/account/login' className='btn-secondary btn-icon'>
-                                <FaSignInAlt /> Login
-                        </Link>
-                    </li>
+                    {user ? (
+                        // If logged in
+                        <>
+                            <li>
+                                <Link href='/events/add'>
+                                    Add Event
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href='/account/dashboard'>
+                                    Dashboard
+                                </Link>
+                            </li>
+                            <li>
+                                <button
+                                    onClick={() => logout()}
+                                    className='btn-secondary btn-icon'
+                                >
+                                    <FaSignOutAlt /> Logout
+                                </button>
+                            </li>
+                        </>
+                    ) : (
+                        // If logged out
+                        <>
+                            <li>
+                                <Link href='/account/login' className='btn-secondary btn-icon'>
+                                        <FaSignInAlt /> Login
+                                </Link>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </header>
