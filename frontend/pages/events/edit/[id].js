cf3ad4f3f50de360f4ac4deb2b7a94d82708b509
Alek Novkovski <alek@greatcode2.com>
2023-02-26 10:41:20 +0100

    Add image preview w dummy 'set image' button

--- frontend/pages/events/edit/[id].js
+++ frontend/pages/events/edit/[id].js
@@ -8,6 +8,7 @@ import Image from 'next/image'
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config'
 import styles from '@/styles/Form.module.css'
+import {FaImage} from "react-icons/fa";
 
 export default function EditEventPage({ evt }) {
     console.log(evt.slug)
@@ -20,6 +21,10 @@ export default function EditEventPage({ evt }) {
         time: evt.time,
         description: evt.description,
     })
+
+    const [imagePreview, setImagePreview] = useState(
+        evt.image ? evt.image.data.attributes.formats.thumbnail.url : null
+    )
 
     const router = useRouter()
 
@@ -138,6 +143,23 @@ export default function EditEventPage({ evt }) {
 
                 <input type='submit' value='Update Event' className='btn' />
             </form>
+
+
+            <h2>Event Image</h2>
+            {imagePreview ? (
+                <Image src={imagePreview} height={100} width={170}  alt={evt.name}/>
+            ) : (
+                <div>
+                    <p>No image uploaded</p>
+                </div>
+            )}
+
+
+            <div>
+                <button className='btn-secondary btn-icon'>
+                    <FaImage /> Set Image
+                </button>
+            </div>
         </Layout>
     )
 }
