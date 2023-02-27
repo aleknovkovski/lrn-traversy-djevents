4a42270f708e0472b0e8be33f2e49ad39b0859d6
Alek Novkovski <alek@greatcode2.com>
2023-02-27 14:17:27 +0100

    Map over events, show names for now; add css

--- frontend/pages/account/dashboard.js
+++ frontend/pages/account/dashboard.js
@@ -1,9 +1,39 @@
+import { parseCookies } from '@/helpers'
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config'
+import classes from './Dashboard.module.css'
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
+    console.log(events)
     return (
         <Layout title='User Dashboard'>
-            <h1>Dashboard</h1>
+            <div className={classes.dash}>
+                <h1>Dashboard</h1>
+                <h3>My Events</h3>
+
+                {events.map((evt) => (
+                    <p key={evt.id}>Event: {evt.name}</p>
+                ))}
+            </div>
         </Layout>
     )
+}
+
+export async function getServerSideProps({ req }) {
+    const { token } = parseCookies(req)
+
+    const res = await fetch(`${API_URL}/myevents`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+
+    const events = await res.json()
+    console.log(events)
+    return {
+        props: {
+            events,
+        },
+    }
 }
