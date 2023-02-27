8c7dbb7bc0ce197370e55e962a9e6e7045460538
Alek Novkovski <alek@greatcode2.com>
2023-02-27 13:56:17 +0100

    Build out myevents backend strapi route

--- backend/src/api/event/controllers/event.js
+++ backend/src/api/event/controllers/event.js
@@ -6,4 +6,24 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController('api::event.event', ({ strapi }) => ({
+  async myEvents(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { error: { message: "No authorization header was found" } },
+      ]);
+    }
+
+    const data = await strapi.db.query('api::event.event').findMany({
+      where: {user: user.id}
+    });
+
+    if (!data) {
+      return ctx.notFound();
+    }
+
+    return data;
+  },
+}));
