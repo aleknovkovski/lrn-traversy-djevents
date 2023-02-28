3dd503281f88c89089e63849b144e9290c4f8485
Alek Novkovski <alek@greatcode2.com>
2023-02-28 01:07:10 +0100

    Add authorized delete on Dboard delete handler

--- frontend/pages/account/dashboard.js
+++ frontend/pages/account/dashboard.js
@@ -3,11 +3,30 @@ import Layout from '@/components/Layout'
 import { API_URL } from '@/config'
 import classes from './Dashboard.module.css'
 import DashboardEvent from "@/components/DashboardEvent";
+import {toast} from "react-toastify";
+import {useRouter} from "next/router";
 
-export default function DashboardPage({ events }) {
-    console.log(events)
-    function deleteEvent(eventId) {
-        console.log('to delete event with id: ' + eventId)
+export default function DashboardPage({events, token}) {
+
+    const router = useRouter()
+
+    async function deleteEvent(eventId) {
+        if (confirm('Are you sure?')) {
+            const res = await fetch(`${API_URL}/events/${eventId}`, {
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+
+            const data = await res.json()
+
+            if (!res.ok) {
+                toast.error(data.message)
+            } else {
+                router.reload()
+            }
+        }
     }
 
     return (
@@ -35,10 +54,11 @@ export async function getServerSideProps({ req }) {
     })
 
     const events = await res.json()
-    console.log(events)
+
     return {
         props: {
             events,
+            token
         },
     }
 }
